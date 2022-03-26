615b12fbb3bb2a8cafbaba471cf606e279efb027
juanjesuspy <juanjesuspy@gmail.com>
2022-03-26 15:20:32 -0300

    added notifications in notifications component

--- src/components/Notifications/Notifications.vue
+++ src/components/Notifications/Notifications.vue
@@ -2,19 +2,8 @@
   <section class="notifications navbar-notifications">
     <header class="cardHeader card-header">
       <div class="text-center mb-sm">
-        <strong>You have 13 notifications</strong>
+        <strong>You have {{notificationsCount}} notifications</strong>
       </div>
-      <b-button-group id="notification-b-buttons">
-        <b-button variant="default" @click="changeNotificationsTab(1)"
-          >Notifications</b-button
-        >
-        <b-button variant="default" @click="changeNotificationsTab(2)"
-          >Messages</b-button
-        >
-        <b-button variant="default" @click="changeNotificationsTab(3)"
-          >Progress</b-button
-        >
-      </b-button-group>
     </header>
     <NewNotifictionsList v-if="newNotifications" />
     <NotifictionsList v-else-if="notificationsTabSelected === 1" />
@@ -22,20 +11,6 @@
     <Progress v-else-if="notificationsTabSelected === 3" />
     <NotifictionsList v-else />
     <footer class="cardFooter text-sm card-footer">
-      <span class="fs-mini">Synced at: 21 Apr 2014 18:36</span>
-      <b-button
-        variant="link"
-        @click="loadNotifications"
-        :class="{
-          disabled: isLoad,
-          'btnNotificationsReload btn-xs float-right py-0': true,
-        }"
-      >
-        <span v-if="isLoad"
-          ><i class="la la-refresh la-spin" /> Loading...</span
-        >
-        <i v-else class="la la-refresh" />
-      </b-button>
     </footer>
   </section>
 </template>
@@ -47,6 +22,8 @@ import NotifictionsList from "./NotificationsDemo/NotificationsList";
 import NewNotifictionsList from "./NotificationsDemo/NewNotificationsList";
 import Messages from "./NotificationsDemo/Messages";
 import Progress from "./NotificationsDemo/Progress";
+import { mapActions } from 'vuex'
+
 
 export default {
   name: "Notification",
@@ -61,9 +38,18 @@ export default {
       notificationsTabSelected: 1,
       newNotifications: null,
       isLoad: false,
+      notificationsCount:0
     };
   },
+  mounted(){
+    this.getUserData().then(() => {
+      this.notificationsCount = this.$store.state.user.userData.notifications.length;
+    })
+  },
   methods: {
+    ...mapActions({
+      getUserData: 'user/getData',
+    }),
     changeNotificationsTab(tab) {
       Vue.set(this, "notificationsTabSelected", tab);
       Vue.set(this, "newNotifications", null);
